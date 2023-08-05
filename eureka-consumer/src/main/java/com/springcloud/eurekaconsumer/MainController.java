a1c9b8ddb13c588d278d1014c28ca144160330e7
ZhangMingShuo <1170709161@qq.com>
2023-08-05 19:31:45 +0800

    RestTemplate 资源重定向的调用使用postForLocation方法  添加这行代码 response.addHeader("Location",uri.toString());

--- eureka-consumer/src/main/java/com/springcloud/eurekaconsumer/MainController.java
+++ eureka-consumer/src/main/java/com/springcloud/eurekaconsumer/MainController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.cloud.netflix.eureka.EurekaServiceInstance;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
